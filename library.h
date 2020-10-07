b5eab631b2d579bab44d1faf2cb2952fdbb1e179
Atharva Mokashi <atharvamokashi31@gmail.com>
2020-10-07 15:42:23 +0530

    skeleton code

---
+++ library.h
@@ -0,0 +1,34 @@
+#ifndef _LIBRARY_H
+#define _LIBRARY_H 
+
+struct user{
+
+};
+
+struct book{
+
+};
+
+struct bookcopy{
+
+};
+
+struct issuerrecord{
+
+};
+
+struct payment{
+
+};
+
+//owner functions
+
+//librarian functions
+
+//member functions
+
+//common functions
+
+#endif
+
+
---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "library.h"
+
+int main()
+{
+    printf("Hello Library!!\n");
+
+    return 0;
+
+}
